bb42a0028566582f9b042b44bbe3bf3289f6e78d
fasseg <frank.asseg@googlemail.com>
2014-04-25 13:20:16 +0200

    added auditservice unit test

---
+++ larch-server/src/test/java/net/objecthunter/larch/service/elasticsearch/ElasticSearchAuditServiceTest.java
@@ -0,0 +1,115 @@
+/* 
+* Copyright 2014 Frank Asseg
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License. 
+*/
+package net.objecthunter.larch.service.elasticsearch;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.objecthunter.larch.helpers.AuditRecords;
+import net.objecthunter.larch.model.security.User;
+import org.easymock.EasyMock;
+import org.elasticsearch.action.ListenableActionFuture;
+import org.elasticsearch.action.get.GetRequestBuilder;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Arrays;
+
+import static org.easymock.EasyMock.*;
+
+public class ElasticSearchAuditServiceTest {
+
+    private ElasticSearchAuditService auditService;
+
+    private Client mockClient;
+
+    @Before
+    public void setup() {
+        auditService = new ElasticSearchAuditService();
+        mockClient = createMock(Client.class);
+        ReflectionTestUtils.setField(auditService, "client", mockClient);
+        ReflectionTestUtils.setField(auditService, "mapper", new ObjectMapper());
+        User u = new User();
+        u.setName("test");
+        u.setPwhash("test");
+        Authentication auth = new UsernamePasswordAuthenticationToken(u, "test");
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    @Test
+    public void testRetrieve() throws Exception {
+        SearchRequestBuilder mockSearchRequestBuilder = createMock(SearchRequestBuilder.class);
+        ListenableActionFuture mockFuture = createMock(ListenableActionFuture.class);
+        SearchResponse mockResponse = createMock(SearchResponse.class);
+        SearchHit[] hitArray = new SearchHit[1];
+        SearchHit hitMock = createMock(SearchHit.class);
+        hitArray[0] = hitMock;
+        SearchHits mockHits = createMock(SearchHits.class);
+
+        expect(mockClient.prepareSearch(ElasticSearchAuditService.INDEX_AUDIT)).andReturn
+                (mockSearchRequestBuilder);
+        expect(mockSearchRequestBuilder.setQuery((MatchQueryBuilder) EasyMock.anyObject())).andReturn
+                (mockSearchRequestBuilder);
+        expect(mockSearchRequestBuilder.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)).andReturn(mockSearchRequestBuilder);
+        expect(mockSearchRequestBuilder.setFrom(0)).andReturn(mockSearchRequestBuilder);
+        expect(mockSearchRequestBuilder.setSize(0)).andReturn(mockSearchRequestBuilder);
+        expect(mockSearchRequestBuilder.execute()).andReturn(mockFuture);
+        expect(mockFuture.actionGet()).andReturn(mockResponse);
+        expect(mockResponse.getHits()).andReturn(mockHits);
+        expect(mockHits.iterator()).andReturn(Arrays.asList(hitArray).iterator());
+        expect(hitMock.getSourceAsString()).andReturn("{}");
+
+        replay(mockClient, mockSearchRequestBuilder, mockFuture, mockHits, mockResponse, hitMock);
+        this.auditService.retrieve("id", 0, 0);
+        verify(mockClient, mockSearchRequestBuilder, mockFuture, mockHits, mockResponse, hitMock);
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        GetRequestBuilder mockGetRequestBuilder = createMock(GetRequestBuilder.class);
+        ListenableActionFuture mockFuture = createMock(ListenableActionFuture.class);
+        GetResponse mockResponse = createMock(GetResponse.class);
+        IndexRequestBuilder mockIndexRequestBuilder = createMock(IndexRequestBuilder.class);
+
+        expect(mockClient.prepareGet(eq(ElasticSearchAuditService.INDEX_AUDIT), isNull(), anyString())).andReturn(mockGetRequestBuilder);
+        expect(mockGetRequestBuilder.execute()).andReturn(mockFuture);
+        expect(mockFuture.actionGet()).andReturn(mockResponse);
+        expect(mockResponse.isExists()).andReturn(false);
+
+        expect(mockClient.prepareIndex(eq(ElasticSearchAuditService.INDEX_AUDIT), eq("audit"),
+                anyString())).andReturn(mockIndexRequestBuilder);
+        expect(mockIndexRequestBuilder.setSource((byte[]) anyObject())).andReturn(mockIndexRequestBuilder);
+        expect(mockIndexRequestBuilder.execute()).andReturn(mockFuture);
+        expect(mockFuture.actionGet()).andReturn(null);
+
+
+        replay(mockClient, mockGetRequestBuilder, mockFuture, mockResponse, mockIndexRequestBuilder);
+        auditService.create(AuditRecords.createEntityRecord("id"));
+        verify(mockClient, mockGetRequestBuilder, mockFuture, mockResponse, mockIndexRequestBuilder);
+    }
+}
