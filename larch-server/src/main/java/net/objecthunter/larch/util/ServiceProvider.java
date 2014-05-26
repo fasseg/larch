f2db9e0ea7ad89178ed56ded5771d937fe1cd3d3
fasseg <frank.asseg@googlemail.com>
2014-05-26 13:38:39 +0200

    added javadocs

--- larch-server/src/main/java/net/objecthunter/larch/util/ServiceProvider.java
+++ larch-server/src/main/java/net/objecthunter/larch/util/ServiceProvider.java
@@ -21,15 +21,26 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 
+/**
+ * A service provider for the various CraSH shell groovy commands. This is used since the {@link org.springframework.beans.factory.annotation.Autowired} annotation
+ * does not seem to work to inject services in the groovy scripts
+ */
 public abstract class ServiceProvider {
     private static final Logger log = LoggerFactory.getLogger(ServiceProvider.class);
 
 
+    /**
+     * Request a Service of a given type from Spring's {@link org.springframework.context.ApplicationContext}
+     *
+     * @param ctx  the {@link org.crsh.command.InvocationContext} passed to e.g. the groovy shell script method
+     * @param type the type of the Service requested
+     * @return A service implementation for the given type
+     */
     public static <T extends Object> T getService(InvocationContext ctx, Class<T> type) {
         final BeanFactory bf = (BeanFactory) ctx.getAttributes().get("spring.beanfactory");
         try {
             return bf.getBean(type);
-        }catch (NoSuchBeanDefinitionException e) {
+        } catch (NoSuchBeanDefinitionException e) {
             log.error("Unable to provide bean of type {}", type.getName());
             return null;
         }
