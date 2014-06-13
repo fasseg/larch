5f3ab82859b56afcc96ee0ed5ab9d15ac3e816f8
fasseg <frank.asseg@googlemail.com>
2014-06-13 14:40:58 +0200

    added enabling/disabling property for mail service

--- larch-server/src/main/java/net/objecthunter/larch/service/impl/DefaultMailService.java
+++ larch-server/src/main/java/net/objecthunter/larch/service/impl/DefaultMailService.java
@@ -17,6 +17,8 @@ package net.objecthunter.larch.service.impl;
 
 import net.objecthunter.larch.model.security.UserRequest;
 import net.objecthunter.larch.service.MailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 
@@ -24,6 +26,7 @@ import javax.annotation.PostConstruct;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
@@ -31,15 +34,23 @@ import java.io.IOException;
 import java.util.Properties;
 
 
-public class DefaultMailService implements MailService{
+public class DefaultMailService implements MailService {
+    private static final Logger log = LoggerFactory.getLogger(DefaultMailService.class);
+
     private InternetAddress mailFrom;
     private String smtpHost;
+    private boolean enabled;
 
     @Autowired
     private Environment env;
 
     @PostConstruct
     public void init() {
+        if (env.getProperty("larch.mail.enabled") != null &&
+                env.getProperty("larch.mail.enabled").equalsIgnoreCase("false")){
+            /* mail sending disabled */
+            return;
+        }
         try {
             final String value = env.getProperty("larch.mail.from");
             if (value.isEmpty()) {
@@ -47,6 +58,7 @@ public class DefaultMailService implements MailService{
             }
             mailFrom = new InternetAddress(value);
             smtpHost = env.getProperty("larch.mail.smtp.host", "localhost");
+            enabled = Boolean.parseBoolean(env.getProperty("larch.mail.enabled","false"));
         } catch (AddressException e) {
             throw new IllegalArgumentException(e);
         }
@@ -54,17 +66,22 @@ public class DefaultMailService implements MailService{
 
     @Override
     public void sendUserRequest(UserRequest req) throws IOException {
+        if (!enabled) {
+            throw new IOException("Mail service is disabled unable to comply with request");
+        }
         final Properties props = System.getProperties();
         props.setProperty("mail.smtp.host", smtpHost);
         final Session sess = Session.getDefaultInstance(props);
         MimeMessage msg = new MimeMessage(sess);
         try {
             msg.setFrom(mailFrom);
-            msg.addRecipient(Message.RecipientType.TO,new InternetAddress(req.getUser().getEmail()));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(req.getUser().getEmail()));
             msg.setSubject("User confirmation for larch repository");
             msg.setText("Please open the following link to confirm your account creation:\n" +
                     "http://localhost:8080/confirm?token=" + req.getToken());
-        }catch (MessagingException e) {
+            Transport.send(msg);
+            log.info("Sent confirmation email to " + req.getUser().getEmail());
+        } catch (MessagingException e) {
             throw new IOException(e);
         }
     }
