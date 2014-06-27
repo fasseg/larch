1fb840bb819f91db869308f3d760a0b92b204d75
mih <michael.hoppe@fiz-karlsruhe.de>
2014-06-27 12:56:55 +0200

    add elasticsearch mappings configuration

--- larch-server/src/main/java/net/objecthunter/larch/service/backend/elasticsearch/AbstractElasticSearchService.java
+++ larch-server/src/main/java/net/objecthunter/larch/service/backend/elasticsearch/AbstractElasticSearchService.java
@@ -1,17 +1,32 @@
 package net.objecthunter.larch.service.backend.elasticsearch;
 
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.io.IOUtils;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
 import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
 import org.elasticsearch.client.Client;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * Abstract service for providing utilitxy methods used by all the different ElasticSearch services
+ * Abstract service for providing utility methods used by all the different ElasticSearch services
  */
 public class AbstractElasticSearchService {
 
     @Autowired
+    protected Environment env;
+
+    @Autowired
     protected Client client;
+
+    @Autowired
+    protected ObjectMapper mapper;
 
     protected void refreshIndex(String... indices) {
         client.admin().indices().refresh(new RefreshRequest(indices)).actionGet();
@@ -19,7 +34,21 @@ public class AbstractElasticSearchService {
 
     protected void checkAndOrCreateIndex(String indexName) {
         if (!indexExists(indexName)) {
-            client.admin().indices().prepareCreate(indexName).execute().actionGet();
+            Map mappings = getMappings(indexName);
+            if (mappings != null && !mappings.isEmpty()) {
+                CreateIndexRequestBuilder requestBuilder = client.admin().indices().prepareCreate(indexName);
+                for (String key : ((Set<String>) mappings.keySet())) {
+                    try {
+                        requestBuilder.addMapping(key, mapper.writeValueAsString(mappings.get(key)));
+                    }
+                    catch (Exception e) {
+                    }
+                }
+                requestBuilder.execute().actionGet();
+            }
+            else {
+                client.admin().indices().prepareCreate(indexName).execute().actionGet();
+            }
         }
     }
 
@@ -29,5 +58,22 @@ public class AbstractElasticSearchService {
 
     protected void waitForIndex(String indexName) {
         this.client.admin().cluster().prepareHealth(indexName).setWaitForYellowStatus().execute().actionGet();
+    }
+
+    private Map getMappings(String indexName) {
+        System.out.println(env.getProperty("elasticsearch.config.path") + indexName + ".json");
+        InputStream in =
+            this.getClass().getResourceAsStream(
+                env.getProperty("elasticsearch.config.path") + indexName + "_mappings.json");
+        if (in != null) {
+            try {
+                String mappings = IOUtils.toString(in);
+                return mapper.readValue(mappings, Map.class);
+            }
+            catch (Exception e) {
+                return null;
+            }
+        }
+        return null;
     }
 }
--- larch-server/src/main/java/net/objecthunter/larch/service/backend/elasticsearch/ElasticSearchAuditService.java
+++ larch-server/src/main/java/net/objecthunter/larch/service/backend/elasticsearch/ElasticSearchAuditService.java
@@ -1,39 +1,19 @@
 /* 
-* Copyright 2014 Frank Asseg
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License. 
-*/
+ * Copyright 2014 Frank Asseg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
 package net.objecthunter.larch.service.backend.elasticsearch;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import net.objecthunter.larch.model.AuditRecord;
-import net.objecthunter.larch.service.backend.BackendAuditService;
-
-import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.log4j.Logger;
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
-import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
-import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.SearchHit;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.annotation.PostConstruct;
 
 import java.io.IOException;
 import java.time.ZoneOffset;
@@ -41,11 +21,29 @@ import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import net.objecthunter.larch.model.AuditRecord;
+import net.objecthunter.larch.service.backend.BackendAuditService;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.log4j.Logger;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.index.query.FilterBuilders;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * Implementation of an {@link net.objecthunter.larch.service.backend.BackendAuditService} built on top of ElasticSearch
  */
 public class ElasticSearchAuditService extends AbstractElasticSearchService implements BackendAuditService {
     public static final String INDEX_AUDIT = "audit";
+
     private static final Logger log = Logger.getLogger(ElasticSearchAuditService.class);
 
     private int maxRecords = 50;
@@ -63,14 +61,14 @@ public class ElasticSearchAuditService extends AbstractElasticSearchService impl
     @Override
     public List<AuditRecord> retrieve(String entityId, int offset, int numRecords) throws IOException {
         numRecords = numRecords > maxRecords ? maxRecords : numRecords;
-        final SearchResponse resp = this.client.prepareSearch(INDEX_AUDIT)
-                .setQuery(QueryBuilders.matchQuery("entityId", entityId))
-                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                .setFrom(offset)
-                .setSize(numRecords)
-                .execute()
+        final SearchResponse resp =
+            this.client
+                .prepareSearch(INDEX_AUDIT)
+                .setQuery(
+                    QueryBuilders.filteredQuery(QueryBuilders.matchAllQuery(),
+                        FilterBuilders.termFilter("entityId", entityId)))
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH).setFrom(offset).setSize(numRecords).execute()
                 .actionGet();
-
 
         final List<AuditRecord> records = new ArrayList<>(numRecords);
         for (final SearchHit hit : resp.getHits()) {
@@ -85,20 +83,17 @@ public class ElasticSearchAuditService extends AbstractElasticSearchService impl
         String id;
         do {
             id = RandomStringUtils.randomAlphabetic(16);
-        } while (this.exists(id));
+        }
+        while (this.exists(id));
         rec.setId(id);
         rec.setTimestamp(ZonedDateTime.now(ZoneOffset.UTC).toString());
-        final IndexResponse resp = this.client.prepareIndex(INDEX_AUDIT, "audit", id)
-                .setSource(mapper.writeValueAsBytes(rec))
-                .execute()
-                .actionGet();
+        final IndexResponse resp =
+            this.client
+                .prepareIndex(INDEX_AUDIT, "audit", id).setSource(mapper.writeValueAsBytes(rec)).execute().actionGet();
         return id;
     }
 
     private boolean exists(String id) {
-        return this.client.prepareGet(INDEX_AUDIT, null, id)
-                .execute()
-                .actionGet()
-                .isExists();
+        return this.client.prepareGet(INDEX_AUDIT, null, id).execute().actionGet().isExists();
     }
 }
