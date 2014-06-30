1d6314c7064c3b379f4c653683f818844d6fc404
fasseg <frank.asseg@objecthunter.net>
2014-06-30 22:24:11 +0200

    added test skeleton

---
+++ larch-integration-fs/src/test/java/net/objecthunter/larch/integration/ElasticSearchCredentialsServiceIT.java
@@ -0,0 +1,63 @@
+package net.objecthunter.larch.integration;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/*
+* Copyright 2014 Frank Asseg
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+public class ElasticSearchCredentialsServiceIT {
+    @Test
+    public void testAddUser() {
+
+    }
+
+    @Test
+    public void testAddGroup() {
+
+    }
+
+    @Test
+    public void testEditUser() {
+
+    }
+
+    @Test
+    public void testRemoveUser() {
+
+    }
+
+    @Test
+    public void testAddUserNoEmail() {
+
+    }
+
+    @Test
+    public void testAddUserNoUsername() {
+
+    }
+
+    @Test
+    public void testEditUserNoEmail() {
+
+    }
+
+    @Test
+    public void testEditUserNoUsername() {
+
+    }
+
+
+}
