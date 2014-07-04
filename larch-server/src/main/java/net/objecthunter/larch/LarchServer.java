e02732d046bfd12a25516811bd936632c516856b
mih <michael.hoppe@fiz-karlsruhe.de>
2014-07-04 12:03:57 +0200

    refactor

--- larch-server/src/main/java/net/objecthunter/larch/LarchServer.java
+++ larch-server/src/main/java/net/objecthunter/larch/LarchServer.java
@@ -32,7 +32,8 @@ public class LarchServer {
         new SpringApplicationBuilder()
                 .showBanner(false)
                 .web(true)
-                .sources(LarchServerConfiguration.class, OAuth2ServerConfig.class, SecurityConfiguration.class)
+                .sources(LarchServerConfiguration.class, OAuth2ServerConfiguration.class,
+                        WebSecurityConfiguration.class)
                 .run(args);
     }
 }
