080ba084f5fba1aaba1b7e83b2513267f84a0d3d
fasseg <frank.asseg@googlemail.com>
2014-07-07 14:43:09 +0200

    added all the properties to the settings class

--- larch-common/src/main/java/net/objecthunter/larch/model/Settings.java
+++ larch-common/src/main/java/net/objecthunter/larch/model/Settings.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. 
  */
+
 package net.objecthunter.larch.model;
 
 import net.objecthunter.larch.model.state.BlobstoreState;
@@ -20,10 +21,524 @@ import net.objecthunter.larch.model.state.IndexState;
 import net.objecthunter.larch.model.state.LarchState;
 
 public class Settings {
+
     private BlobstoreState blobstoreState;
+
     private IndexState indexState;
+
     private LarchState larchState;
+
     private Describe describe;
+
+    private String larchClusterName;
+
+    private String larchVersion;
+
+    private boolean larchExportEnabled;
+
+    private String larchExportPath;
+
+    private boolean larchCsrfEnabled;
+
+    private boolean larchMessagingEnabled;
+
+    private String larchMessagingBrokerUri;
+
+    private String larchMessagingBrokerPath;
+
+    private boolean larchMailEnabled;
+
+    private String larchMailFrom;
+
+    private String larchMailSmtpHost;
+
+    private int larchMailSmtpPort;
+
+    private String larchMailSmtpUser;
+
+    private String larchMailSmtpPass;
+
+    private String elasticSearchClusterName;
+
+    private String elasticSearchLogPath;
+
+    private String elasticSearchDataPath;
+
+    private String elasticSearchBootstrapMlockAll;
+
+    private String elasticSearchBindHost;
+
+    private int elasticSearchExpectedNodes;
+
+    private int elasticSearchHttpPort;
+
+    private boolean elasticSearchHttpEnabled;
+
+    private String elasticSearchGatwayType;
+
+    private String elasticSearchConfigPath;
+
+    private String springActiveProfile;
+
+    private String springShowBanner;
+
+    private String larchLogPath;
+
+    private String larchLogFile;
+
+    private int larchServerPort;
+
+    private boolean tomcatAccessLogEnabled;
+
+    private boolean jsonPrettyPrint;
+
+    private String thymeleafPrefix;
+
+    private String thymeleafSuffix;
+
+    private String thymeleafMode;
+
+    private String thymeleafEncoding;
+
+    private boolean thymeleafCacheEnabled;
+
+    private boolean springJmxEnabled;
+
+    private boolean springEndpointAutoconfigEnabled;
+
+    private boolean springEndpointBeansEnabled;
+
+    private boolean springEndpointConfigPropsEnabled;
+
+    private boolean springEndpointDumpEnabled;
+
+    private boolean springEndpointEnvEnabled;
+
+    private boolean springEndpointHealthEnabled;
+
+    private boolean springEndpointInfoEnabled;
+
+    private boolean springEndpointMetricsEnabled;
+
+    private boolean springEndpointShutdownEnabled;
+
+    private boolean springEndpointTraceEnabled;
+
+    private boolean springEndpointJolokiaEnabled;
+
+    private boolean springEndpointJMXEnabled;
+
+    private boolean springShellEnabled;
+
+    private String springShellPathPatterns;
+
+    public String getElasticSearchDataPath() {
+        return elasticSearchDataPath;
+    }
+
+    public void setElasticSearchDataPath(String elasticSearchDataPath) {
+        this.elasticSearchDataPath = elasticSearchDataPath;
+    }
+
+    public String getLarchClusterName() {
+        return larchClusterName;
+    }
+
+    public void setLarchClusterName(String larchClusterName) {
+        this.larchClusterName = larchClusterName;
+    }
+
+    public String getLarchVersion() {
+        return larchVersion;
+    }
+
+    public void setLarchVersion(String larchVersion) {
+        this.larchVersion = larchVersion;
+    }
+
+    public boolean isLarchExportEnabled() {
+        return larchExportEnabled;
+    }
+
+    public void setLarchExportEnabled(boolean larchExportEnabled) {
+        this.larchExportEnabled = larchExportEnabled;
+    }
+
+    public String getLarchExportPath() {
+        return larchExportPath;
+    }
+
+    public void setLarchExportPath(String larchExportPath) {
+        this.larchExportPath = larchExportPath;
+    }
+
+    public boolean isLarchCsrfEnabled() {
+        return larchCsrfEnabled;
+    }
+
+    public void setLarchCsrfEnabled(boolean larchCsrfEnabled) {
+        this.larchCsrfEnabled = larchCsrfEnabled;
+    }
+
+    public boolean isLarchMessagingEnabled() {
+        return larchMessagingEnabled;
+    }
+
+    public void setLarchMessagingEnabled(boolean larchMessagingEnabled) {
+        this.larchMessagingEnabled = larchMessagingEnabled;
+    }
+
+    public String getLarchMessagingBrokerUri() {
+        return larchMessagingBrokerUri;
+    }
+
+    public void setLarchMessagingBrokerUri(String larchMessagingBrokerUri) {
+        this.larchMessagingBrokerUri = larchMessagingBrokerUri;
+    }
+
+    public String getLarchMessagingBrokerPath() {
+        return larchMessagingBrokerPath;
+    }
+
+    public void setLarchMessagingBrokerPath(String larchMessagingBrokerPath) {
+        this.larchMessagingBrokerPath = larchMessagingBrokerPath;
+    }
+
+    public boolean isLarchMailEnabled() {
+        return larchMailEnabled;
+    }
+
+    public void setLarchMailEnabled(boolean larchMailEnabled) {
+        this.larchMailEnabled = larchMailEnabled;
+    }
+
+    public String getLarchMailFrom() {
+        return larchMailFrom;
+    }
+
+    public void setLarchMailFrom(String larchMailFrom) {
+        this.larchMailFrom = larchMailFrom;
+    }
+
+    public String getLarchMailSmtpHost() {
+        return larchMailSmtpHost;
+    }
+
+    public void setLarchMailSmtpHost(String larchMailSmtpHost) {
+        this.larchMailSmtpHost = larchMailSmtpHost;
+    }
+
+    public int getLarchMailSmtpPort() {
+        return larchMailSmtpPort;
+    }
+
+    public void setLarchMailSmtpPort(int larchMailSmtpPort) {
+        this.larchMailSmtpPort = larchMailSmtpPort;
+    }
+
+    public String getLarchMailSmtpUser() {
+        return larchMailSmtpUser;
+    }
+
+    public void setLarchMailSmtpUser(String larchMailSmtpUser) {
+        this.larchMailSmtpUser = larchMailSmtpUser;
+    }
+
+    public String getLarchMailSmtpPass() {
+        return larchMailSmtpPass;
+    }
+
+    public void setLarchMailSmtpPass(String larchMailSmtpPass) {
+        this.larchMailSmtpPass = larchMailSmtpPass;
+    }
+
+    public String getElasticSearchClusterName() {
+        return elasticSearchClusterName;
+    }
+
+    public void setElasticSearchClusterName(String elasticSearchClusterName) {
+        this.elasticSearchClusterName = elasticSearchClusterName;
+    }
+
+    public String getElasticSearchLogPath() {
+        return elasticSearchLogPath;
+    }
+
+    public void setElasticSearchLogPath(String elasticSearchLogPath) {
+        this.elasticSearchLogPath = elasticSearchLogPath;
+    }
+
+    public String getElasticSearchBootstrapMlockAll() {
+        return elasticSearchBootstrapMlockAll;
+    }
+
+    public void setElasticSearchBootstrapMlockAll(String elasticSearchBootstrapMlockAll) {
+        this.elasticSearchBootstrapMlockAll = elasticSearchBootstrapMlockAll;
+    }
+
+    public String getElasticSearchBindHost() {
+        return elasticSearchBindHost;
+    }
+
+    public void setElasticSearchBindHost(String elasticSearchBindHost) {
+        this.elasticSearchBindHost = elasticSearchBindHost;
+    }
+
+    public int getElasticSearchExpectedNodes() {
+        return elasticSearchExpectedNodes;
+    }
+
+    public void setElasticSearchExpectedNodes(int elasticSearchExpectedNodes) {
+        this.elasticSearchExpectedNodes = elasticSearchExpectedNodes;
+    }
+
+    public int getElasticSearchHttpPort() {
+        return elasticSearchHttpPort;
+    }
+
+    public void setElasticSearchHttpPort(int elasticSearchHttpPort) {
+        this.elasticSearchHttpPort = elasticSearchHttpPort;
+    }
+
+    public boolean isElasticSearchHttpEnabled() {
+        return elasticSearchHttpEnabled;
+    }
+
+    public void setElasticSearchHttpEnabled(boolean elasticSearchHttpEnabled) {
+        this.elasticSearchHttpEnabled = elasticSearchHttpEnabled;
+    }
+
+    public String getElasticSearchGatwayType() {
+        return elasticSearchGatwayType;
+    }
+
+    public void setElasticSearchGatwayType(String elasticSearchGatwayType) {
+        this.elasticSearchGatwayType = elasticSearchGatwayType;
+    }
+
+    public String getElasticSearchConfigPath() {
+        return elasticSearchConfigPath;
+    }
+
+    public void setElasticSearchConfigPath(String elasticSearchConfigPath) {
+        this.elasticSearchConfigPath = elasticSearchConfigPath;
+    }
+
+    public String getSpringActiveProfile() {
+        return springActiveProfile;
+    }
+
+    public void setSpringActiveProfile(String springActiveProfile) {
+        this.springActiveProfile = springActiveProfile;
+    }
+
+    public String getSpringShowBanner() {
+        return springShowBanner;
+    }
+
+    public void setSpringShowBanner(String springShowMainBanner) {
+        this.springShowBanner = springShowMainBanner;
+    }
+
+    public String getLarchLogPath() {
+        return larchLogPath;
+    }
+
+    public void setLarchLogPath(String larchLogPath) {
+        this.larchLogPath = larchLogPath;
+    }
+
+    public String getLarchLogFile() {
+        return larchLogFile;
+    }
+
+    public void setLarchLogFile(String larchLogFile) {
+        this.larchLogFile = larchLogFile;
+    }
+
+    public int getLarchServerPort() {
+        return larchServerPort;
+    }
+
+    public void setLarchServerPort(int larchServerPort) {
+        this.larchServerPort = larchServerPort;
+    }
+
+    public boolean isTomcatAccessLogEnabled() {
+        return tomcatAccessLogEnabled;
+    }
+
+    public void setTomcatAccessLogEnabled(boolean tomcatAccessLogEnabled) {
+        this.tomcatAccessLogEnabled = tomcatAccessLogEnabled;
+    }
+
+    public boolean isJsonPrettyPrint() {
+        return jsonPrettyPrint;
+    }
+
+    public void setJsonPrettyPrint(boolean jsonPrettyPrint) {
+        this.jsonPrettyPrint = jsonPrettyPrint;
+    }
+
+    public String getThymeleafPrefix() {
+        return thymeleafPrefix;
+    }
+
+    public void setThymeleafPrefix(String thymeleafPrefix) {
+        this.thymeleafPrefix = thymeleafPrefix;
+    }
+
+    public String getThymeleafSuffix() {
+        return thymeleafSuffix;
+    }
+
+    public void setThymeleafSuffix(String thymeleafSuffix) {
+        this.thymeleafSuffix = thymeleafSuffix;
+    }
+
+    public String getThymeleafMode() {
+        return thymeleafMode;
+    }
+
+    public void setThymeleafMode(String thymeleafMode) {
+        this.thymeleafMode = thymeleafMode;
+    }
+
+    public String getThymeleafEncoding() {
+        return thymeleafEncoding;
+    }
+
+    public void setThymeleafEncoding(String thymeleafEncoding) {
+        this.thymeleafEncoding = thymeleafEncoding;
+    }
+
+    public boolean isThymeleafCacheEnabled() {
+        return thymeleafCacheEnabled;
+    }
+
+    public void setThymeleafCacheEnabled(boolean thymeleafCacheEnabled) {
+        this.thymeleafCacheEnabled = thymeleafCacheEnabled;
+    }
+
+    public boolean isSpringJmxEnabled() {
+        return springJmxEnabled;
+    }
+
+    public void setSpringJmxEnabled(boolean springJmxEnabled) {
+        this.springJmxEnabled = springJmxEnabled;
+    }
+
+    public boolean isSpringEndpointAutoconfigEnabled() {
+        return springEndpointAutoconfigEnabled;
+    }
+
+    public void setSpringEndpointAutoconfigEnabled(boolean springEndpointAutoconfigEnabled) {
+        this.springEndpointAutoconfigEnabled = springEndpointAutoconfigEnabled;
+    }
+
+    public boolean isSpringEndpointBeansEnabled() {
+        return springEndpointBeansEnabled;
+    }
+
+    public void setSpringEndpointBeansEnabled(boolean springEndpointBeansEnabled) {
+        this.springEndpointBeansEnabled = springEndpointBeansEnabled;
+    }
+
+    public boolean isSpringEndpointConfigPropsEnabled() {
+        return springEndpointConfigPropsEnabled;
+    }
+
+    public void setSpringEndpointConfigPropsEnabled(boolean springEndpointConfigPropsEnabled) {
+        this.springEndpointConfigPropsEnabled = springEndpointConfigPropsEnabled;
+    }
+
+    public boolean isSpringEndpointDumpEnabled() {
+        return springEndpointDumpEnabled;
+    }
+
+    public void setSpringEndpointDumpEnabled(boolean springEndpointDumpEnabled) {
+        this.springEndpointDumpEnabled = springEndpointDumpEnabled;
+    }
+
+    public boolean isSpringEndpointEnvEnabled() {
+        return springEndpointEnvEnabled;
+    }
+
+    public void setSpringEndpointEnvEnabled(boolean springEndpointEnvEnabled) {
+        this.springEndpointEnvEnabled = springEndpointEnvEnabled;
+    }
+
+    public boolean isSpringEndpointHealthEnabled() {
+        return springEndpointHealthEnabled;
+    }
+
+    public void setSpringEndpointHealthEnabled(boolean springEndpointHealthEnabled) {
+        this.springEndpointHealthEnabled = springEndpointHealthEnabled;
+    }
+
+    public boolean isSpringEndpointInfoEnabled() {
+        return springEndpointInfoEnabled;
+    }
+
+    public void setSpringEndpointInfoEnabled(boolean springEndpointInfoEnabled) {
+        this.springEndpointInfoEnabled = springEndpointInfoEnabled;
+    }
+
+    public boolean isSpringEndpointMetricsEnabled() {
+        return springEndpointMetricsEnabled;
+    }
+
+    public void setSpringEndpointMetricsEnabled(boolean springEndpointMetricsEnabled) {
+        this.springEndpointMetricsEnabled = springEndpointMetricsEnabled;
+    }
+
+    public boolean isSpringEndpointShutdownEnabled() {
+        return springEndpointShutdownEnabled;
+    }
+
+    public void setSpringEndpointShutdownEnabled(boolean springEndpointShutdownEnabled) {
+        this.springEndpointShutdownEnabled = springEndpointShutdownEnabled;
+    }
+
+    public boolean isSpringEndpointTraceEnabled() {
+        return springEndpointTraceEnabled;
+    }
+
+    public void setSpringEndpointTraceEnabled(boolean springEndpointTraceEnabled) {
+        this.springEndpointTraceEnabled = springEndpointTraceEnabled;
+    }
+
+    public boolean isSpringEndpointJolokiaEnabled() {
+        return springEndpointJolokiaEnabled;
+    }
+
+    public void setSpringEndpointJolokiaEnabled(boolean springEndpointJolokiaEnabled) {
+        this.springEndpointJolokiaEnabled = springEndpointJolokiaEnabled;
+    }
+
+    public boolean isSpringEndpointJMXEnabled() {
+        return springEndpointJMXEnabled;
+    }
+
+    public void setSpringEndpointJMXEnabled(boolean springEndpointJMXEnabled) {
+        this.springEndpointJMXEnabled = springEndpointJMXEnabled;
+    }
+
+    public boolean isSpringShellEnabled() {
+        return springShellEnabled;
+    }
+
+    public void setSpringShellEnabled(boolean springShellEnabled) {
+        this.springShellEnabled = springShellEnabled;
+    }
+
+    public String getSpringShellPathPatterns() {
+        return springShellPathPatterns;
+    }
+
+    public void setSpringShellPathPatterns(String springShellPathPatterns) {
+        this.springShellPathPatterns = springShellPathPatterns;
+    }
 
     public Describe getDescribe() {
         return describe;
--- larch-server/src/main/java/net/objecthunter/larch/controller/SettingsController.java
+++ larch-server/src/main/java/net/objecthunter/larch/controller/SettingsController.java
@@ -13,13 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package net.objecthunter.larch.controller;
+
+import java.io.IOException;
 
 import net.objecthunter.larch.model.Settings;
 import net.objecthunter.larch.service.RepositoryService;
 import net.objecthunter.larch.service.backend.BackendBlobstoreService;
 import net.objecthunter.larch.service.backend.BackendEntityService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -29,14 +34,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.IOException;
-
 /**
  * Web Controller responsible for Settings views
  */
 @RequestMapping("/settings")
 @Controller
 public class SettingsController {
+
     @Autowired
     private RepositoryService repositoryService;
 
@@ -46,9 +50,12 @@ public class SettingsController {
     @Autowired
     private BackendEntityService entityService;
 
+    @Autowired
+    private Environment environment;
+
     /**
      * Retrieve the settings overview page from the repository
-     *
+     * 
      * @throws IOException
      */
     @RequestMapping(method = RequestMethod.GET, produces = "text/html")
@@ -74,6 +81,81 @@ public class SettingsController {
         settings.setDescribe(this.repositoryService.describe());
         settings.setIndexState(this.entityService.status());
         settings.setBlobstoreState(this.blobstoreService.status());
-        return settings;
+        settings.setLarchClusterName(environment.getProperty("larch.cluster.name"));
+        settings.setLarchVersion(environment.getProperty("larch.version"));
+        settings.setLarchExportEnabled(
+                Boolean.parseBoolean(environment.getProperty("larch.export.auto")));
+        settings.setLarchExportPath(environment.getProperty("larch.export.path"));
+        settings.setLarchCsrfEnabled(
+                Boolean.parseBoolean(environment.getProperty("larch.security.csrf.enabled")));
+        settings.setLarchMessagingEnabled(
+                Boolean.parseBoolean(environment.getProperty("larch.messaging.enabled")));
+        settings.setLarchMessagingBrokerUri(environment.getProperty("larch.messaging.broker.uri"));
+        settings.setLarchMessagingBrokerPath(environment.getProperty("larch.messaging.path.data"));
+        settings.setLarchMailEnabled(
+                Boolean.parseBoolean(environment.getProperty("larch.mail.enabled")));
+        settings.setLarchMailFrom(environment.getProperty("larch.mail.from"));
+        settings.setLarchMailSmtpHost(environment.getProperty("larch.mail.smtp.host"));
+        settings.setLarchMailSmtpPort(
+                Integer.parseInt(environment.getProperty("larch.mail.smtp.port")));
+        settings.setLarchMailSmtpUser(environment.getProperty("larch.mail.smtp.user"));
+        settings.setLarchMailSmtpPass(environment.getProperty("larch.mail.smtp.pass"));
+        settings.setElasticSearchClusterName(environment.getProperty("elasticsearch.cluster.name"));
+        settings.setElasticSearchLogPath(environment.getProperty("elasticsearch.path.logs"));
+        settings.setElasticSearchDataPath(environment.getProperty("elasticsearch.path.data"));
+        settings.setElasticSearchBootstrapMlockAll(
+                environment.getProperty("elasticsearch.bootstrap.mlockall"));
+        settings.setElasticSearchBindHost(
+                environment.getProperty("elasticsearch.network.bind.host"));
+        settings.setElasticSearchExpectedNodes(
+                Integer.parseInt(environment.getProperty("elasticsearch.gateway.expected_nodes")));
+        settings.setElasticSearchHttpPort(
+                Integer.parseInt(environment.getProperty("elasticsearch.http.port")));
+        settings.setElasticSearchHttpEnabled(
+                Boolean.parseBoolean(environment.getProperty("elasticsearch.http.enabled")));
+        settings.setElasticSearchGatwayType(environment.getProperty("elasticsearch.gateway.type"));
+        settings.setElasticSearchConfigPath(environment.getProperty("elasticsearch.config.path"));
+        settings.setSpringActiveProfile(environment.getProperty("spring.profiles.active"));
+        settings.setSpringShowBanner(environment.getProperty("spring.main.show-banner"));
+        settings.setLarchLogPath(environment.getProperty("logging.path"));
+        settings.setLarchLogFile(environment.getProperty("logging.file"));
+        settings.setLarchServerPort(Integer.parseInt(environment.getProperty("server.port")));
+        settings.setTomcatAccessLogEnabled(
+                Boolean.parseBoolean(environment.getProperty("server.tomcat.access-log-enabled")));
+        settings.setJsonPrettyPrint(
+                Boolean.parseBoolean(environment.getProperty("http.mappers.json-pretty-print")));
+        settings.setThymeleafPrefix(environment.getProperty("spring.thymeleaf.prefix"));
+        settings.setThymeleafSuffix(environment.getProperty("spring.thymeleaf.suffix"));
+        settings.setThymeleafMode(environment.getProperty("spring.thymeleaf.mode"));
+        settings.setThymeleafEncoding(environment.getProperty("spring.thymeleaf.encoding"));
+        settings.setThymeleafCacheEnabled(
+                Boolean.parseBoolean(environment.getProperty("spring.thymeleaf.cache")));
+        settings.setSpringJmxEnabled(
+                Boolean.parseBoolean(environment.getProperty("spring.jmx.enabled")));
+        settings.setSpringEndpointAutoconfigEnabled(
+                Boolean.parseBoolean(environment.getProperty("endpoints.autoconfig.enabled")));
+        settings.setSpringEndpointBeansEnabled(
+                Boolean.parseBoolean(environment.getProperty("endpoints.beans.enabled")));
+        settings.setSpringEndpointConfigPropsEnabled(
+                Boolean.parseBoolean(environment.getProperty("endpoints.configprops.enabled")));
+        settings.setSpringEndpointDumpEnabled(
+                Boolean.parseBoolean(environment.getProperty("endpoints.dump.enabled")));
+        settings.setSpringEndpointEnvEnabled(
+                Boolean.parseBoolean(environment.getProperty("endpoints.env.enabled")));
+        settings.setSpringEndpointHealthEnabled(Boolean.parseBoolean(environment
+                .getProperty("endpoints.health.enabled")));
+        settings.setSpringEndpointInfoEnabled(Boolean.parseBoolean(environment.getProperty("endpoints.info.enabled")));
+        settings.setSpringEndpointMetricsEnabled(Boolean.parseBoolean(environment
+                .getProperty("endpoints.metrics.enabled")));
+        settings.setSpringEndpointShutdownEnabled(Boolean.parseBoolean(environment
+                .getProperty("endpoints.shutdown.enabled")));
+        settings.setSpringEndpointTraceEnabled(Boolean.parseBoolean(environment
+                .getProperty("endpoints.trace.enabled")));
+        settings.setSpringEndpointJolokiaEnabled(Boolean.parseBoolean(environment
+                .getProperty("endpoints.jolokia.enabled")));
+        settings.setSpringEndpointJMXEnabled(Boolean.parseBoolean(environment.getProperty("endpoints.jmx.enabled")));
+        settings.setSpringShellEnabled(Boolean.parseBoolean(environment.getProperty("shell.ssh.enabled")));
+        settings.setSpringShellPathPatterns(environment.getProperty("shell.commandPathPatterns"));
+      return settings;
     }
 }
