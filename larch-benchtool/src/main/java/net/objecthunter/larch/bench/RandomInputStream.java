aa57d7f9153a2116ff6cd3b0db37ffb6aa99383a
fasseg <frank.asseg@googlemail.com>
2014-07-08 19:33:33 +0200

    removed some logging output from random inputs tream and disabled export and messaging by default

--- larch-benchtool/src/main/java/net/objecthunter/larch/bench/RandomInputStream.java
+++ larch-benchtool/src/main/java/net/objecthunter/larch/bench/RandomInputStream.java
@@ -40,7 +40,6 @@ public class RandomInputStream extends InputStream {
     @Override
     public int read() throws IOException {
         if (bytesRead == size) {
-            log.info("random stream stopping after " + bytesRead + " bytes");
             return -1;
         }
         if (slicePos >= BenchTool.SLICE.length) {
